faeb78688a733aab01445dcc26f5a83ad0a08fd5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-09 17:46:37 +0500

    Create add_balance function

--- role/user/user.py
+++ role/user/user.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from main_files.decorator_func import log_decorator
-from main_files.json_manager import user_manager
+from main_files.json_manager import user_manager, balance_manager
 
 
 class User:
@@ -8,12 +10,35 @@ class User:
 
     @log_decorator
     def summ_count(self):
-        pass
+        try:
+            summ = 0
+            all_balance = balance_manager.read()
+            for balance in all_balance:
+                summ += balance
+            return summ
+        except KeyError:
+            return 0
+        except IndexError:
+            return 0
+        except Exception as e:
+            print(f'Error: {e}')
+            return False
 
     @log_decorator
     def add_balance(self):
         try:
+            print(f'Your balance is {self.summ_count()}')
             balance: int = int(input("Enter balance: "))
+            data = {
+                'balance': balance,
+                'phone_number': self.active_user['phone_number'],
+                'create_data': datetime.now().strftime("%d/%m/%Y %H:%M:%S").__str__()
+            }
+            if balance_manager.append_data(data=data):
+                print(f'{balance} has been added to your balance')
+                return True
+            print("Something went wrong")
+            return False
         except Exception as e:
             print(f'Error: {e}')
             return False
