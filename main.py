b3ed4f65682dadfd69c276869c2b01ebdf8b6487
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-09 17:27:25 +0500

    Create and change files

--- main.py
+++ main.py
@@ -10,31 +10,89 @@ def show_auth():
 3. Quit
     '''
     print(text)
-    choose_menu = int(input("Choose menu number: "))
-    if choose_menu == 1:
-        auth.register()
-        show_auth()
-    elif choose_menu == 2:
-        login = auth.login()
-        if login is None:
-            print("Login Failed")
+    try:
+        choose_menu = int(input("Choose menu number: "))
+        if choose_menu == 1:
+            auth.register()
             show_auth()
-        elif not login['is_login']:
-            show_auth()
-        elif login['role'] == 'admin':
-            pass
-        elif login['role'] == 'user':
-            pass
+        elif choose_menu == 2:
+            login = auth.login()
+            if login is None:
+                print("Login Failed")
+                show_auth()
+            elif not login['is_login']:
+                show_auth()
+            elif login['role'] == 'admin':
+                pass
+            elif login['role'] == 'user':
+                pass
+            else:
+                print("Something went wrong")
+                show_auth()
+        elif choose_menu == 3:
+            print("Good bye!")
+            auth.logout()
+            return
         else:
-            print("Something went wrong")
+            print("Wrong menu number")
             show_auth()
-    elif choose_menu == 3:
-        print("Good bye!")
-        auth.logout()
-        return
-    else:
+    except ValueError:
         print("Wrong menu number")
         show_auth()
+    except Exception as e:
+        print(f'Error: {e}')
+        show_auth()
+
+
+@log_decorator
+def show_user_menu():
+    text = '''
+1. Add product to balance
+2. History balance
+3. History product
+    '''
+    print(text)
+    try:
+        user_menu = int(input("Choose menu number: "))
+        if user_menu == 1:
+            pass
+        elif user_menu == 2:
+            pass
+        elif user_menu == 3:
+            pass
+        else:
+            print("Wrong menu number")
+            show_user_menu()
+    except ValueError:
+        print("Wrong menu number")
+        show_user_menu()
+
+
+@log_decorator
+def show_admin_menu():
+    text = '''
+1. Pricing
+2. Show sold
+3. Show all users
+    '''
+    print(text)
+    try:
+        user_menu = int(input("Choose menu number: "))
+        if user_menu == 1:
+            pass
+        elif user_menu == 2:
+            pass
+        elif user_menu == 3:
+            pass
+        else:
+            print("Wrong menu number")
+            show_admin_menu()
+    except ValueError:
+        print("Wrong menu number")
+        show_admin_menu()
+    except Exception as e:
+        print(f'Error: {e}')
+        show_admin_menu()
 
 
 if __name__ == '__main__':
