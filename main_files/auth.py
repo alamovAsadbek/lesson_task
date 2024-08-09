4877587fafdb278619f10323f3c53158d549ebeb
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-09 17:10:01 +0500

    Change files

---
+++ main_files/auth.py
@@ -0,0 +1,74 @@
+import hashlib
+
+from main_files.decorator_func import log_decorator
+from main_files.json_manager import user_manager
+
+
+class Auth:
+    def __init__(self):
+        self.__phone_number = "990690142"
+        self.__password = "admin"
+
+    @log_decorator
+    def register(self) -> bool:
+        full_name: str = input('Full name: ').strip()
+        phone_number: int = int(input('Phone number: ').strip())
+        if self.__phone_number == phone_number.__str__():
+            print('Phone number already taken')
+            return False
+        else:
+            if user_manager.check_phone_number(phone_number=phone_number):
+                print('Phone number already taken')
+                return False
+        while True:
+            password = hashlib.sha256(input('Password: ').strip().encode('utf-8')).hexdigest()
+            confirm_password = hashlib.sha256(input("Confirm password: ").encode('utf-8')).hexdigest()
+            if password == confirm_password:
+                break
+            print('Passwords do not match')
+        user_id = user_manager.random_id()
+        data = {
+            'id': user_id,
+            'full_name': full_name,
+            'password': password,
+            'confirm_password': confirm_password,
+            'phone_number': phone_number,
+            'is_login': False
+        }
+        if user_manager.append_data(data=data):
+            print('User registered')
+            return True
+        print('An error occurred')
+        return False
+
+    @log_decorator
+    def login(self) -> dict:
+        all_users: list = user_manager.read()
+        phone_number: int = int(input('Enter phone number: ').strip())
+        password: str = hashlib.sha256(input('Enter password (+998 ): ').strip().encode('utf-8')).hexdigest()
+        if phone_number.__str__() == self.__phone_number and hashlib.sha256(
+                self.__password.encode('utf-8')).hexdigest() == password:
+            print('Login successful')
+            return {'is_login': True, 'role': 'admin'}
+        if not user_manager.check_phone_number(phone_number=phone_number):
+            print('Phone number not found')
+            return {'is_login': False, 'role': "admin"}
+        for user in all_users:
+            if user['phone_number'] == phone_number and user['password'] == password:
+                user['is_login'] = True
+                if user_manager.write(data=all_users):
+                    print('Login successful')
+                    return {'is_login': True, 'role': 'user'}
+        print('Login failed')
+        return {'is_login': False, 'role': 'admin'}
+
+    @log_decorator
+    def logout(self) -> bool:
+        try:
+            all_users: list = user_manager.read()
+            for user in all_users:
+                user['is_login'] = False
+            user_manager.write(all_users)
+            return True
+        except ValueError:
+            return True
---
+++ main_files/json_manager.py
@@ -0,0 +1,83 @@
+import json
+import os
+import random
+
+if not os.path.exists('datas'):
+    os.makedirs('datas')
+
+
+class JsonManager:
+    def __init__(self, file_name):
+        self.file_name: str = file_name
+
+    def read(self) -> dict or bool:
+        try:
+            with open(self.file_name, 'r') as file:
+                data = json.load(file)
+                return data
+        except FileNotFoundError:
+            with open(self.file_name, 'w') as file:
+                json.dump([], file, indent=4)
+                return []
+
+    def write(self, data) -> bool:
+        with open(self.file_name, 'w') as file:
+            json.dump(data, file, indent=4)
+            return True
+
+    def check_phone_number(self, phone_number) -> bool:
+        all_users: list = self.read()
+        try:
+            for user in all_users:
+                if user['phone_number'] == phone_number:
+                    return True
+            return False
+        except KeyError:
+            return False
+
+    def append_data(self, data) -> bool:
+        try:
+            all_data: list = self.read()
+            all_data.append(data)
+            self.write(all_data)
+            return True
+        except Exception as e:
+            print(f'Error: {e}')
+            return False
+
+    def get_active_user(self) -> dict or bool:
+        all_users: list = self.read()
+        try:
+            for user in all_users:
+                if user['is_login']:
+                    return user
+            return False
+        except Exception as e:
+            print(f'Error: {e}')
+            return False
+
+    def get_data(self, data_id) -> dict or bool:
+        all_users: list = self.read()
+        try:
+            for data in all_users:
+                if data['id'] == data_id:
+                    return data
+            return False
+        except KeyError:
+            return False
+
+    def random_id(self):
+        try:
+            all_data: list = self.read()
+            while True:
+                random_number: int = random.randint(1, 9999)
+                for data in all_data:
+                    if data['id'] == random_number:
+                        break
+                return random_number
+        except Exception as e:
+            print(f'Error: {e}')
+            return False
+
+
+user_manager = JsonManager(file_name='datas/users.json')
